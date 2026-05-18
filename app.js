88afd4ae16d258d46898785275a0ddfe46b2050a
AAymoodu <ayomideamodu045@gmail.com>
2026-05-18 15:21:14 +0100

    fix: app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const icon_menu = document.querySelector(".icon-menu");
-// const nav = document.querySelector("header nav");
-// const icon_close = document.querySelector(".iconclose");
-// icon_menu.addEventListener('click',()=>{
-//     nav.style.transform= 'translateX(0)';
-// })
-// icon_close.addEventListener('click',()=>{
-//       nav.style.transform = "translateX(150%)";
-// })
+const nav = document.querySelector("header nav");
+const icon_close = document.querySelector(".iconclose");
+icon_menu.addEventListener('click',()=>{
+    nav.style.transform= 'translateX(0)';
+})
+icon_close.addEventListener('click',()=>{
+      nav.style.transform = "translateX(150%)";
+})
